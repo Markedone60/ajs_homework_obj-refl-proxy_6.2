b6526024c9c20247dea132df9995d2e5abb1a5f3
Grigory <gregory.rvl@yandex.ru>
2022-09-17 20:50:51 +0300

    init commit

---
+++ src/js/__tests__/app.test.js
@@ -0,0 +1,37 @@
+import specialAttack from '../app';
+
+test('testing special attack functionality, should return array with four fields', () => {
+  const character = {
+    name: 'Лучник',
+    type: 'Bowman',
+    health: 50,
+    level: 3,
+    attack: 40,
+    defence: 10,
+    special: [
+      {
+        id: 8,
+        name: 'Двойной выстрел',
+        icon: 'http://...',
+        description: 'Двойной выстрел наносит двойной урон',
+      },
+      {
+        id: 9,
+        name: 'Нокаутирующий удар',
+        icon: 'http://...',
+      },
+    ],
+  };
+
+  const received = specialAttack(character);
+  const expected = [
+    {
+      id: 8, name: 'Двойной выстрел', icon: 'http://...', description: 'Двойной выстрел наносит двойной урон',
+    },
+    {
+      id: 9, name: 'Нокаутирующий удар', icon: 'http://...', description: 'Описание недоступно',
+    },
+  ];
+
+  expect(received).toEqual(expected);
+});
---
+++ src/js/app.js
@@ -0,0 +1,33 @@
+export default function specialAttack(character) {
+  const { special = [] } = character;
+
+  return special.map((attack) => ({
+    description: 'Описание недоступно',
+    ...attack,
+  }));
+}
+
+/*
+const character = {
+  name: 'Лучник',
+  type: 'Bowman',
+  health: 50,
+  level: 3,
+  attack: 40,
+  defence: 10,
+  special: [
+    {
+      id: 8,
+      name: 'Двойной выстрел',
+      icon: 'http://...',
+      description: 'Двойной выстрел наносит двойной урон'
+    },
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      icon: 'http://...'
+      // <- обратите внимание, описание "засекречено"
+    }
+  ]
+}
+*/
